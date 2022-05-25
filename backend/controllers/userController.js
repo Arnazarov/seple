d94bc514af87374d3b4a7abbf22936a687543c13
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-25 00:54:58 -0500

    implemented route for registering a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,8 @@
-
+import User from "../models/userModel.js";
 
 // @desc    Fetch all user
 // @route   GET /api/users
 // @access  Public
-
 export const getAllUsers = (req, res) => {
     res.send({message: 'All Users'});
 }
@@ -12,7 +11,29 @@ export const getAllUsers = (req, res) => {
 // @desc    Register a user
 // @route   POST /api/users
 // @access  Public
+export const registerUser = async (req, res) => {
+    try {
 
-export const registerUser = (req, res) => {
-    res.send({message: 'User Registered'});
+        const {name, email, password } = req.body;
+
+        const userExists = await User.findOne({email});
+
+        if (userExists) {
+            return res.status(400).json({message: 'User already exists'});
+        }
+
+        const user = await User.create({
+            name,
+            password,
+            email,
+        });
+    
+        if (user) {
+            res.status(201).json(user);
+        } else {
+            res.status(400).json('Invalid user data')
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 }
