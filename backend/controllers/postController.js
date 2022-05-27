55f0504987a1e5c6c09a1dd33d0c8f980640d5ff
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-05-27 18:57:37 -0500

    implemented fetch timeline posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,131 +1,139 @@
-import Post from "../models/postModel.js";
+import Post from '../models/postModel.js';
+import User from '../models/userModel.js';
 
-// @desc    Fetch all posts
+// @desc    Fetch timeline posts
 // @route   GET /api/posts
 // @access  Public
 export const fetchPosts = async (req, res) => {
-    try {
+  try {
+    const { userID } = req.body;
 
-        res.status(200).json({message: 'OK'});
+    const currUser = await User.findById(userID);
 
-    } catch (error) {
-        res.status(500).json({message: error.message});
+    if (currUser) {
+      const currUserPosts = await Post.find({ userID: currUser._id });
+      const followingUsersPosts = await Promise.all(
+        currUser.following.map((id) => Post.find({ userID: id }))
+      );
+
+      res.status(200).json(currUserPosts.concat(...followingUsersPosts));
+    } else {
+      res.status(400).json({ message: 'User not found!' });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // @desc    Fetch single post
 // @route   GET /api/posts/:id
 // @access  Public
 export const fetchPost = async (req, res) => {
-    try {
-
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(post);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // @desc    Create a post
 // @route   POST /api/posts
 // @access  Private
 export const createPost = async (req, res) => {
-    try {
+  try {
+    const post = new Post(req.body);
 
-        const post = new Post(req.body);
+    const createdPost = await post.save();
 
-        const createdPost = await post.save();
-
-        res.status(200).json(createdPost);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+    res.status(200).json(createdPost);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // @desc    Update a post
 // @route   PUT /api/posts/:id
 // @access  Private
 export const updatePost = async (req, res) => {
-    try {
+  try {
+    const {
+      params: { id },
+      body: { userID },
+    } = req;
+    const post = await Post.findById(id);
 
-        const {params: {id}, body: {userID}} = req;
-        const post = await Post.findById(id);
+    if (post) {
+      if (userID !== String(post.userID)) {
+        return res
+          .status(403)
+          .json({ message: 'You can modify only your posts!' });
+      }
 
-        if (post) {
+      const newPost = Object.assign(post, req.body);
+      delete newPost.userID;
 
-            if (userID !== String(post.userID)) {
-                return res.status(403).json({message: 'You can modify only your posts!'});
-            }
-
-            const newPost = Object.assign(post, req.body);
-            delete newPost.userID;
-
-            const updatedPost = await newPost.save();
-            res.status(200).json(updatedPost);
-
-        } else {
-            res.status(400).json({message: 'Post does not exist'});
-        }
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
+      const updatedPost = await newPost.save();
+      res.status(200).json(updatedPost);
+    } else {
+      res.status(400).json({ message: 'Post does not exist' });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // @desc    Delete a post
 // @route   DELETE /api/posts/:id
 // @access  Private
 export const deletePost = async (req, res) => {
-    try {
+  try {
+    const {
+      params: { id },
+      body: { userID },
+    } = req;
+    const post = await Post.findById(id);
 
-        const {params: {id}, body: {userID}} = req;
-        const post = await Post.findById(id);
+    if (post) {
+      if (userID !== String(post.userID)) {
+        return res
+          .status(403)
+          .json({ message: 'You can delete only your posts!' });
+      }
 
-        if (post) {
-
-            if (userID !== String(post.userID)) {
-                return res.status(403).json({message: 'You can delete only your posts!'});
-            }
-
-            await post.remove();
-            res.status(200).json({message: 'Post deleted successfully!'});
-            
-        } else {
-            res.status(400).json({message: 'Post does not exist'});
-        }
-
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
+      await post.remove();
+      res.status(200).json({ message: 'Post deleted successfully!' });
+    } else {
+      res.status(400).json({ message: 'Post does not exist' });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // @desc    Like/Unlike a post
 // @route   PUT /api/post/:id/like
 // @access  Private
 export const likePost = async (req, res) => {
-    try {
+  try {
+    const {
+      params: { id },
+      body: { userID },
+    } = req;
+    const post = await Post.findById(id);
 
-        const {params: {id}, body: {userID}} = req;
-        const post = await Post.findById(id);
-
-        if (post) {
-
-            if (post.likes.includes(userID)) {
-                await post.updateOne({$pull: {likes: userID}});
-                res.status(400).json({message: 'You have disliked this post!'});
-            } else {
-                await post.updateOne({$push: {likes: userID}});
-                res.status(200).json({message: 'You have liked this post!'});
-            }
-            
-        } else {
-            res.status(400).json({message: 'Post does not exist'});
-        }
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
+    if (post) {
+      if (post.likes.includes(userID)) {
+        await post.updateOne({ $pull: { likes: userID } });
+        res.status(400).json({ message: 'You have disliked this post!' });
+      } else {
+        await post.updateOne({ $push: { likes: userID } });
+        res.status(200).json({ message: 'You have liked this post!' });
+      }
+    } else {
+      res.status(400).json({ message: 'Post does not exist' });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
